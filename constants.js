ff324c12414bdfcba812435cc49666b3b097a05a
Alex <160778418+Alex000115@users.noreply.github.com>
2026-01-19 14:15:38 +0600

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,7 @@
+// Global configuration for the scripts
+module.exports = {
+    PROPOSAL_DESCRIPTION: "Fund Development Team Phase 1",
+    TARGET_AMOUNT: "0.5", // ETH
+    VOTING_DELAY: 0, 
+    MIN_QUORUM: 100
+};
